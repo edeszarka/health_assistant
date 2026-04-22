9a4201595639bde032315e771f9e29fcaa7922a7
edeszarka <ede.szarka@gmail.com>
2026-04-22 17:05:10 +0200

    Refactor: adding local host instead of wildcard

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:8501"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
